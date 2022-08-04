cf2ffa9bd637f71dfacef667989ac404a626a7a7
ian <ian@DESKTOP-RCHQUBT.localdomain>
2022-08-04 15:03:14 -0400

    ajout image a la page accueil

---
+++ src/components/container/accueil.js
@@ -0,0 +1,17 @@
+import { Box,  Image} from "@chakra-ui/react"
+
+
+export function Acceuil(props)  {
+
+    return (
+
+            <Image src='https://valleyfield.imgix.net/headers/valleyfield-nautisme-deny-cardinal.jpg?auto=format&crop=focalpoint&domain=valleyfield.imgix.net&fit=crop&fp-x=0.5&fp-y=0.5&h=421&ixlib=php-3.3.0&q=82&w=1500&s=a437de86b3e32a18dc202192747955d9'
+                    
+                    
+                    alt='Bienvenu'
+            />
+      
+    )
+
+
+}
