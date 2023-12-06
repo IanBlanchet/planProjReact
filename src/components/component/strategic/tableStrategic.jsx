21f7e053510b16e0bee040bc659f317ad9ed639f
ianbla <tag.ianblanchet@gmail.com>
2023-12-06 15:17:34 -0500

    remove annee in filter for strategic files

--- src/components/component/strategic/tableStrategic.jsx
+++ src/components/component/strategic/tableStrategic.jsx
@@ -76,7 +76,7 @@ export function TableStrategic({user}) {
         
         getRessources('/api/v1/projet').then( projets => {                     
             let filterProjet = projets.filter(item => item.nature&&item.nature.isStrategic);                        
-            filterProjet = filterProjet.filter(item => item.nature&&(new Date(item.nature.echeance).getFullYear()) === 2023);
+            //filterProjet = filterProjet.filter(item => item.nature&&(new Date(item.nature.echeance).getFullYear()) === 2023);
             filterProjet = filterProjet.filter(item => item.statut === 'Actif' ||item.statut === 'En approbation');
             filterProjet = filterProjet.sort((a,b) => {
                 if (a.nature.echeance < b.nature.echeance){
